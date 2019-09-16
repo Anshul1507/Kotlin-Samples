a5c1acdece3ea6f2e93cb7861d17a603a3494b21
Anshul1507 <ag1507anshul@gmail.com>
2019-09-16 15:01:06 +0530

    Added Dice Roller Code

---
+++ DiceRoller/app/src/main/java/com/github_kotlin/udacity_dice_roller/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.github_kotlin.udacity_dice_roller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+    lateinit var rollButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rollButton = findViewById(R.id.button)
+        diceImage = findViewById(R.id.dice_image)
+
+        rollButton.text = "Let's Roll"
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+
+        val randomInt = Random().nextInt(6) + 1
+
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+    }
+}
