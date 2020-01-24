bd4dee6c86dd32526e972b8628a1dc1a0fa44fca
Anshul1507 <ag1507anshul@gmail.com>
2020-01-24 19:54:12 +0530

    Guess it :: added GameViewModel
    First view model architecture

--- GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,11 +17,14 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -30,6 +33,8 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
  * Fragment where the game is played
  */
 class GameFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
 
     // The current word
     private var word = ""
@@ -52,7 +57,8 @@ class GameFragment : Fragment() {
                 container,
                 false
         )
-
+        Log.i("GameFragment","GameFragment viewModel Called!")
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
         resetList()
         nextWord()
 
