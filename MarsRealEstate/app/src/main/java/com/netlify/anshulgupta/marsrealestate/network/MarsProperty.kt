87a105ff8a4b4916b598cd4949491ab85ea95ac9
Anshul1507 <ag1507anshul@gmail.com>
2020-05-29 02:45:54 +0530

    MarsApp :: Integrate moshi in place of Scalars and GSON converter factory

--- MarsRealEstate/app/src/main/java/com/netlify/anshulgupta/marsrealestate/network/MarsProperty.kt
+++ MarsRealEstate/app/src/main/java/com/netlify/anshulgupta/marsrealestate/network/MarsProperty.kt
@@ -1,3 +1,10 @@
 package com.netlify.anshulgupta.marsrealestate.network
 
-class MarsProperty()
+import com.squareup.moshi.Json
+
+data class MarsProperty(
+    val id: String,
+    @Json(name = "img_src")val imgSrcUrl: String,
+    val type: String,
+    val price: Double
+)
--- MarsRealEstate/app/src/main/java/com/netlify/anshulgupta/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate/app/src/main/java/com/netlify/anshulgupta/marsrealestate/overview/OverviewViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.netlify.anshulgupta.marsrealestate.network.MarsApi
+import com.netlify.anshulgupta.marsrealestate.network.MarsProperty
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,13 +22,13 @@ class OverviewViewModel : ViewModel() {
     }
 
     private fun getMarsRealEstateProperties() {
-        MarsApi.retrofitService.getProperties().enqueue( object: Callback<String> {
-            override fun onFailure(call: Call<String>, t: Throwable) {
+        MarsApi.retrofitService.getProperties().enqueue( object: Callback<List<MarsProperty>> {
+            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
                 _response.value = "Failure: " + t.message
             }
 
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                _response.value = response.body()
+            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
+                _response.value = "Success: ${response.body()?.size} Mars properties retrieved!"
             }
         })
 
