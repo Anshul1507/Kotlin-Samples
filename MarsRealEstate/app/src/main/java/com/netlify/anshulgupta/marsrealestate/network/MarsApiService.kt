464d479b572c602c36a868e9c2daf4d05628a88f
Anshul1507 <ag1507anshul@gmail.com>
2020-05-29 02:13:06 +0530

    MarsApp :: added interface for retrofit

--- MarsRealEstate/app/src/main/java/com/netlify/anshulgupta/marsrealestate/network/MarsApiService.kt
+++ MarsRealEstate/app/src/main/java/com/netlify/anshulgupta/marsrealestate/network/MarsApiService.kt
@@ -1,3 +1,24 @@
 package com.netlify.anshulgupta.marsrealestate.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://mars.udacity.com/"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(GsonConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MarsApiService {
+    @GET("realestate")
+    fun getProperties(): Call<String>
+}
+
+object MarsApi{
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
