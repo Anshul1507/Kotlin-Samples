b85f443531e8dabe316f5b251d3ab9c0fcfa89d3
Anshul1507 <ag1507anshul@gmail.com>
2020-05-29 02:30:42 +0530

    MarsApp :: update networking to display data

--- MarsRealEstate/app/src/main/java/com/netlify/anshulgupta/marsrealestate/network/MarsApiService.kt
+++ MarsRealEstate/app/src/main/java/com/netlify/anshulgupta/marsrealestate/network/MarsApiService.kt
@@ -2,13 +2,13 @@ package com.netlify.anshulgupta.marsrealestate.network
 
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(GsonConverterFactory.create())
+    .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
 
--- MarsRealEstate/app/src/main/java/com/netlify/anshulgupta/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate/app/src/main/java/com/netlify/anshulgupta/marsrealestate/overview/OverviewViewModel.kt
@@ -3,6 +3,10 @@ package com.netlify.anshulgupta.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.netlify.anshulgupta.marsrealestate.network.MarsApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class OverviewViewModel : ViewModel() {
@@ -15,7 +19,18 @@ class OverviewViewModel : ViewModel() {
     init {
         getMarsRealEstateProperties()
     }
+
     private fun getMarsRealEstateProperties() {
+        MarsApi.retrofitService.getProperties().enqueue( object: Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure: " + t.message
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
+            }
+        })
+
         _response.value = "Set the Mars API Response here!"
     }
 }
