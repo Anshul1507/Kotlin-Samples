5917efc2f03660fbad450b2e13e11c74aa90ecb0
Anshul1507 <ag1507anshul@gmail.com>
2020-05-31 04:22:20 +0530

    NCDemo :: Added Fragment Screens

---
+++ NavigationComponentDemo/app/src/main/java/com/netlify/anshulgupta/navigationcomponentdemo/ChooseRecipientFragment.kt
@@ -0,0 +1,15 @@
+package com.netlify.anshulgupta.navigationcomponentdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ChooseRecipientFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_choose_recipient, container, false)
+    }
+
+}
