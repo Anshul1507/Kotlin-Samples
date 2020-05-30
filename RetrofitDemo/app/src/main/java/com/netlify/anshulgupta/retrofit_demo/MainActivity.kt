d4009325b4da5761598ecac5566dcaf6ef12d612
Anshul1507 <ag1507anshul@gmail.com>
2020-05-30 15:18:42 +0530

    Retroft :: formatting output data

--- RetrofitDemo/app/src/main/java/com/netlify/anshulgupta/retrofit_demo/MainActivity.kt
+++ RetrofitDemo/app/src/main/java/com/netlify/anshulgupta/retrofit_demo/MainActivity.kt
@@ -3,12 +3,11 @@ package com.netlify.anshulgupta.retrofit_demo
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.telecom.Call
-import com.netlify.anshulgupta.retrofit_demo.DataClasses.User
 import com.netlify.anshulgupta.retrofit_demo.DataClasses.UserItem
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,7 +24,16 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onResponse(call: retrofit2.Call<List<UserItem>>,response: Response<List<UserItem>>) {
-                text_data.text = response.body().toString()
+                var string: StringBuilder = StringBuilder()
+                val dataList = response.body()!!
+                for(user in dataList){
+                    string.append(user.id.toString() + " ")
+                    string.append(user.name + "\n")
+                    string.append(user.email_address + "\n")
+                    string.append(user.address.toString() + "\n")
+                    string.append(user.phone + "\n\n")
+                }
+                text_data.text = string
             }
         })
 
