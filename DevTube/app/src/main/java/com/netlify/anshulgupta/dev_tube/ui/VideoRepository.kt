6b83f5b878a5d208ffddabb47525662c100dbdd8
Anshul1507 <ag1507anshul@gmail.com>
2020-06-01 05:16:03 +0530

    DevTube :: added the videos into local db using repos

--- DevTube/app/src/main/java/com/netlify/anshulgupta/dev_tube/ui/VideoRepository.kt
+++ DevTube/app/src/main/java/com/netlify/anshulgupta/dev_tube/ui/VideoRepository.kt
@@ -1,2 +1,17 @@
 package com.netlify.anshulgupta.dev_tube.ui
 
+import com.netlify.anshulgupta.dev_tube.db.VideoDB
+import com.netlify.anshulgupta.dev_tube.network.Network
+import com.netlify.anshulgupta.dev_tube.network.asDataBaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/* Repository for fetching videos from the network and storing them on disk.*/
+class VideoRepository (private val db:VideoDB){
+    suspend fun refreshVideos(){
+        withContext(Dispatchers.IO){
+            val playlist = Network.devTube.getPlayList()
+            db.videoDao.insertAll(*playlist.asDataBaseModel()) //storing into db
+        }
+    }
+}
