da5ceeab64b0085d0500c0dad24a882bb69cc908
Anshul1507 <ag1507anshul@gmail.com>
2020-06-01 04:20:21 +0530

    DevTube :: Refactor DAO with livedata

--- DevTube/app/src/main/java/com/netlify/anshulgupta/dev_tube/db/Room.kt
+++ DevTube/app/src/main/java/com/netlify/anshulgupta/dev_tube/db/Room.kt
@@ -1,5 +1,6 @@
 package com.netlify.anshulgupta.dev_tube.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -8,7 +9,7 @@ import androidx.room.Query
 @Dao
 interface VideoDao {
     @Query("SELECT * FROM dbvideo")
-    fun getVideos() : List<DBVideo>
+    fun getVideos() : LiveData<List<DBVideo>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DBVideo)
