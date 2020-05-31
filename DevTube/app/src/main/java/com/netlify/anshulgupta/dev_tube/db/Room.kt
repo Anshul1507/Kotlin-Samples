a482fe99ce064445d03fc1ce74fd22bd900ca2b2
Anshul1507 <ag1507anshul@gmail.com>
2020-06-01 05:07:17 +0530

    DevTube :: update Video DB

--- DevTube/app/src/main/java/com/netlify/anshulgupta/dev_tube/db/Room.kt
+++ DevTube/app/src/main/java/com/netlify/anshulgupta/dev_tube/db/Room.kt
@@ -1,10 +1,8 @@
 package com.netlify.anshulgupta.dev_tube.db
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface VideoDao {
@@ -13,4 +11,21 @@ interface VideoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DBVideo)
+}
+
+@Database(entities = [DBVideo::class], version = 1)
+abstract class VideoDB: RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideoDB
+
+fun getDB(context: Context): VideoDB {
+
+    if(!::INSTANCE.isInitialized){
+        //when instance is not initialized -> create
+        INSTANCE = Room.databaseBuilder(context.applicationContext,VideoDB::class.java,"videos").build()
+    }
+
+    return INSTANCE
 }
