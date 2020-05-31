01cc64bbc62387082e075f271201b263269e0e1b
Anshul1507 <ag1507anshul@gmail.com>
2020-06-01 04:18:41 +0530

    DevTube :: Added DAO for the video DB

--- DevTube/app/src/main/java/com/netlify/anshulgupta/dev_tube/db/Room.kt
+++ DevTube/app/src/main/java/com/netlify/anshulgupta/dev_tube/db/Room.kt
@@ -1,2 +1,15 @@
 package com.netlify.anshulgupta.dev_tube.db
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface VideoDao {
+    @Query("SELECT * FROM dbvideo")
+    fun getVideos() : List<DBVideo>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DBVideo)
+}
