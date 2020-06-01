ac32a3533b9dc90975b21d51128744a5cbc288be
Anshul1507 <ag1507anshul@gmail.com>
2020-06-01 05:37:23 +0530

    DevTube :: init work manager

--- DevTube/app/src/main/java/com/netlify/anshulgupta/dev_tube/work/RefreshDataWork.kt
+++ DevTube/app/src/main/java/com/netlify/anshulgupta/dev_tube/work/RefreshDataWork.kt
@@ -1,2 +1,24 @@
 package com.netlify.anshulgupta.dev_tube.work
 
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.Operation.SUCCESS
+import androidx.work.WorkerParameters
+import com.netlify.anshulgupta.dev_tube.db.getDB
+import com.netlify.anshulgupta.dev_tube.ui.Video
+import com.netlify.anshulgupta.dev_tube.ui.VideoRepository
+import retrofit2.HttpException
+
+class RefreshDataWork (appContext: Context, params: WorkerParameters): CoroutineWorker(appContext,params) {
+    override suspend fun doWork(): Result {
+        val database = getDB(applicationContext)
+        val repository = VideoRepository(database)
+
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+}
