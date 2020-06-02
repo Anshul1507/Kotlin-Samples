184a416b6fdfeeeaca20879f83ebfbdca59c4d5c
Anshul1507 <ag1507anshul@gmail.com>
2020-06-02 08:49:57 +0530

    GDGFinder :: Added Navigation Drawer & setup navigation & homepage

--- GDGFinder/app/src/main/java/com/netlify/anshulgupta/gdg_finder/home/HomeFragment.kt
+++ GDGFinder/app/src/main/java/com/netlify/anshulgupta/gdg_finder/home/HomeFragment.kt
@@ -5,19 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.netlify.anshulgupta.gdg_finder.R
 
-/**
- * A simple [Fragment] subclass.
- */
 class HomeFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_home,container,false)
+
+        viewModel = ViewModelProvider (this).get(HomeViewModel::class.java)
+
+        return view
     }
 
 }
