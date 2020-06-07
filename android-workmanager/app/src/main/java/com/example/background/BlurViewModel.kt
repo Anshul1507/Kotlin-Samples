50dbe8da005a6145ae447681aedfd58c1988da56
Anshul1507 <ag1507anshul@gmail.com>
2020-06-07 09:00:33 +0530

    WorkManager :: Starter Codelabs code

---
+++ android-workmanager/app/src/main/java/com/example/background/BlurViewModel.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.background
+
+import android.app.Application
+import android.net.Uri
+import androidx.lifecycle.AndroidViewModel
+
+
+class BlurViewModel(application: Application) : AndroidViewModel(application) {
+
+    internal var imageUri: Uri? = null
+    internal var outputUri: Uri? = null
+
+    /**
+     * Create the WorkRequest to apply the blur and save the resulting image
+     * @param blurLevel The amount to blur the image
+     */
+    internal fun applyBlur(blurLevel: Int) {}
+
+    private fun uriOrNull(uriString: String?): Uri? {
+        return if (!uriString.isNullOrEmpty()) {
+            Uri.parse(uriString)
+        } else {
+            null
+        }
+    }
+
+    /**
+     * Setters
+     */
+    internal fun setImageUri(uri: String?) {
+        imageUri = uriOrNull(uri)
+    }
+
+    internal fun setOutputUri(outputImageUri: String?) {
+        outputUri = uriOrNull(outputImageUri)
+    }
+}
