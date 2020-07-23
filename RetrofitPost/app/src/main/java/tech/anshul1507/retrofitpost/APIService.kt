60dff7d269f3beef46088627ad6e69c985c86fae
Anshul1507 <ag1507anshul@gmail.com>
2020-07-24 02:57:22 +0530

    RetrofitPost :: Refactored Code

--- RetrofitPost/app/src/main/java/tech/anshul1507/retrofitpost/APIService.kt
+++ RetrofitPost/app/src/main/java/tech/anshul1507/retrofitpost/APIService.kt
@@ -1,12 +1,29 @@
 package tech.anshul1507.retrofitpost
 
+import okhttp3.OkHttpClient
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
 
+private const val BASE_URL = "https://reqres.in/"
+
+private val client = OkHttpClient.Builder().build()
+
+private val retrofitBuilder = Retrofit.Builder()
+    .addConverterFactory(GsonConverterFactory.create())
+    .baseUrl("https://reqres.in/")
+    .client(client)
+    .build()
+
 interface APIService {
-
     @POST("api/login")
-    fun sendUserData(@Body userPosts: Model): Call<Model>
+    fun sendUserData(@Body userLogin: Model): Call<Model>
+}
 
+object LoginAPI{
+    val retrofitService: APIService by lazy {
+        retrofitBuilder.create(APIService::class.java)
+    }
 }
--- RetrofitPost/app/src/main/java/tech/anshul1507/retrofitpost/MainActivity.kt
+++ RetrofitPost/app/src/main/java/tech/anshul1507/retrofitpost/MainActivity.kt
@@ -1,37 +1,25 @@
 package tech.anshul1507.retrofitpost
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val client = OkHttpClient.Builder().build()
-        val retrofitBuilder = Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl("https://reqres.in/")
-            .client(client)
-            .build()
-
-        val apiService = retrofitBuilder.create(APIService::class.java)
-
         login.setOnClickListener {
             val email = email_tv.text.toString()
             val password = password_tv.text.toString()
 
             val userLogin = Model(email,password)
-            val call = apiService.sendUserData(userLogin)
+            val call = LoginAPI.retrofitService.sendUserData(userLogin)
 
             call.enqueue(object : Callback<Model> {
                 override fun onFailure(call: Call<Model>, t: Throwable) {
